7b1bc2669d450b413b2244d0aed1c266c542ae55
adashko <dashkoalina@yahoo.com>
2023-11-19 22:38:57 +0200

    Added homework for 19. Playwright fixtures

---
+++ src/pageObjects/GaragePage/GaragePage.js
@@ -0,0 +1,9 @@
+import {BasePage} from "../BasePage.js";
+
+export class GaragePage extends BasePage {
+
+    constructor(page) {
+        super(page, '/panel/garage', page.locator('button', {hasText: 'Add car'}));
+        this.addCarButton = page.locator('button.btn.btn-primary');
+    }
+}
