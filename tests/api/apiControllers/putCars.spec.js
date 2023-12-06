c8e5517afb0d6739f25e1dc5ffe72102e5fc3b57
adashko <dashkoalina@yahoo.com>
2023-12-07 00:28:31 +0200

    Added homework for 23. API testing Data Models

--- tests/api/apiControllers/putCars.spec.js
+++ tests/api/apiControllers/putCars.spec.js
@@ -4,11 +4,12 @@ import { VALID_BRANDS_RESPONSE_BODY } from "../../../src/data/dict/brands.js";
 import { VALID_BRAND_MODELS } from "../../../src/data/dict/models.js";
 import { USERS } from "../../../src/data/dict/users.js";
 import { APIClient } from "../../../src/client/APIClient.js";
+import { EditCarModel} from "../../../src/models/cars/putCarsModels.js";
+import { CreateCarModel} from "../../../src/models/cars/postCarsModel.js";
 
 test.describe("API PUT Tests", () => {
     let client;
     let carId;
-    let requestBody;
 
     test.beforeAll(async () => {
         client = await APIClient.authenticate(undefined, {
@@ -17,21 +18,31 @@ test.describe("API PUT Tests", () => {
             "remember": false
         });
 
-        const newCar = await client.cars.createUserCar(requestBody)
+        const carModel = new CreateCarModel({carBrandId: 1, carModelId: 2, mileage: 133})
+        const brand = VALID_BRANDS_RESPONSE_BODY.data.find((brand) => brand.id === carModel.carBrandId)
+        const model = VALID_BRAND_MODELS[brand.id].data.find((model) => model.id === carModel.carModelId)
+        const newCar = await client.cars.createUserCar(carModel)
         carId = newCar.data.data.id
+        let carInitialMilleage;
+        carInitialMilleage = carModel.mileage
+    })
 
-    test("PUT /cars Should edit a new car", async ({})=>{
-        const brandIdEdit = VALID_BRANDS_RESPONSE_BODY.data[1].id
-        const modelIdEdit = VALID_BRAND_MODELS[brandId].data[1].id;
-        const requestBodyEdit = {
-            "carBrandId": brandIdEdit,
-            "carModelId": modelIdEdit,
-            "mileage": 125
+    test("PUT /cars Should edit a new car", async ({}) => {
+        const editedCarModel = new EditCarModel({carBrandId: 2, carModelId: 4, mileage: 122})
+        const brand = VALID_BRANDS_RESPONSE_BODY.data.find((brand) => brand.id === editedCarModel.carBrandId)
+        const model = VALID_BRAND_MODELS[brand.id].data.find((model) => model.id === editedCarModel.carModelId)
+        const response = await client.cars.editUserCarById(carId, editedCarModel)
+        const expectedBody = {
+            ...editedCarModel,
+            initialMileage: expect.any(Number),
+            id: expect.any(Number),
+            updatedMileageAt: expect.any(String),
+            brand: brand.title,
+            model: model.title,
+            logo: brand.logoFilename
         }
-        const response = await client.cars.editUserCar(carId,requestBodyEdit)
-        expect(response.status, "Status code should be 201").toEqual(201)
+        expect(response.status, "Status code should be 200").toEqual(200)
         expect(response.data.status, "Success response should be returned").toBe("ok")
-        expect(response.data.data, "Should edit a new car").toMatchObject(requestBody)
+        expect(response.data.data, "Should edit a new car").toEqual(expectedBody)
     });
-});
 });
